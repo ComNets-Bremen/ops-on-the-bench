6857c4f5506ea637ea46265c59b681de658db9c3
Asanga Udugama <adu@comnets.uni-bremen.de>
2020-04-16 15:36:04 +0200

    path issue fix

---
+++ manager/utils/utils/queue-sim.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python3
+#
+# Queue a simulation.
+#
+# Author: Asanga Udugama (adu@comnets.uni-bremen.de)
+# Date: 12-April-2020
+#
+import redis
+import rq
+import worker
+
+def main():
+
+    # make string of the omnetpp.ini file
+    with open ('omnetpp.ini', 'r') as inifilefp:
+        inistr = inifilefp.read()
+
+    # make the parameters
+    execution = 1
+    arguments = {
+                'user' : 'adu',
+                'title' : 'Epidemic with SWIM mobility',
+                'omnetpp.ini' : inistr,
+                'runconfig' : 'General'
+                }
+
+    # connect to REDIS
+    redisconn = redis.Redis(host='localhost')
+    queue = rq.Queue(connection=redisconn)
+
+    # queue the simulation
+    queue.enqueue(utils.worker.run_simulation, execution, arguments, job_timeout=43200)
+
+
+if __name__ == "__main__":
+    main()
+
