ed4fd31bd0454d6216220dc5c65fd8a0ed2d87de
Jens Dede <jd@comnets.uni-bremen.de>
2020-05-26 23:31:25 +0200

    Make fields readonly in admin interface it does not make sense to change it

--- manager/omnetppManager/admin.py
+++ manager/omnetppManager/admin.py
@@ -7,7 +7,18 @@ from .models import Simulation, StorageBackend, ConfigKeyValueStorage
 
 # Admin view for Simulations
 class SimulationAdmin(admin.ModelAdmin):
-    readonly_fields = ("simulation_id", "simulation_error", "handled_by",)
+    readonly_fields = (
+            "simulation_id",
+            "summarizing_precision",
+            "simulation_error",
+            "handled_by",
+            "job_error",
+            "meta_full",
+            "shared_link",
+            "storage_backend",
+            "runconfig",
+            "omnetppini",
+            )
 
 admin.site.register(Simulation, SimulationAdmin)
 
