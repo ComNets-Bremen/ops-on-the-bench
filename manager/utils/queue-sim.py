f5d4407c11d4498ca41f1e9830e099c6821f1b7f
Asanga Udugama <adu@uranus.comnets.uni-bremen.de>
2021-07-06 12:00:55 +0200

    queue-sim test program with config file

--- manager/utils/queue-sim.py
+++ manager/utils/queue-sim.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 #
-# Queue a simulation.
+# Queue a simulation. Use the given sample configuration as
+# parameter file.
 #
 # Author: Asanga Udugama (adu@comnets.uni-bremen.de)
 # Date: 12-April-2020
@@ -10,37 +11,50 @@ import rq
 import worker
 import argparse
 
-def main(inifile, runconfig):
+def main(configfile):
+    # init valiables
+    params = {}
+
+    # read config and get parameters
+    with open(configfile) as f:
+        for line in f:
+            if len(line.strip()) == 0 or line.strip().startswith('#'):
+                continue
+            cols = line.split('%%%')
+            if cols[1].strip() == 'string':
+                params[cols[0].strip()] = cols[2].strip()
+            elif cols[1].strip() == 'int':
+                params[cols[0].strip()] = int(cols[2].strip())
+            elif cols[1].strip() == 'float':
+                params[cols[0].strip()] = float(cols[2].strip())
 
     # make string of the omnetpp.ini file
-    with open (inifile, 'r') as inifilefp:
+    with open (params['inifile'], 'r') as inifilefp:
         inistr = inifilefp.read()
 
     # make the parameters
     execution = 1
     arguments = {
-                'user' : 'adu',
-                'title' : runconfig,
+                'user' : params['user'],
+                'title' : params['title'],
                 'omnetpp.ini' : inistr,
-                'runconfig' : runconfig,
-                'summarizing_precision' : 100.0,
-                'storage_backend_id' : 'dropbox',
-                'storage_backend_token' : '68xuOnr4c-AAAAAAAAAAIG1w-UhkxyhddCb9Hu011bedIpjsDwaO0Iujk4XPtcx_'
+                'runconfig' : params['runconfig'],
+                'summarizing_precision' : params['precision'],
+                'storage_backend_id' : params['backend'],
+                'storage_backend_token' : params['backendtoken']
                 }
 
     # connect to REDIS
-    redisconn = redis.Redis(host='localhost')
+    redisconn = redis.Redis(host=params['redishost'], port=params['redisport'], password=params['redispwd'])
     queue = rq.Queue(connection=redisconn)
 
     # queue the simulation
-    queue.enqueue(worker.run_simulation, execution, arguments, job_timeout=259200)
-
+    queue.enqueue(worker.run_simulation, execution, arguments, job_timeout=params['jobtimeout'])
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument('-i', '--inifile', help='.ini file to use', required=True)
-    parser.add_argument('-r', '--runconfig', help='Config tag to use', required=True)
+    parser.add_argument('-c', '--configfile', help='configuration file with all the parameters', required=True)
     args = parser.parse_args()
 
-    main(args.inifile, args.runconfig)
+    main(args.configfile)
 
