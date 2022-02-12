8456a824d30f8d96c1b6192ee431e20672bd8d1a
Oluwapelumi <odedereoluwapelumi13@gmail.com>
2022-02-12 20:39:52 +0100

    user email activation

--- manager/omnetppManager/views.py
+++ manager/omnetppManager/views.py
@@ -19,6 +19,11 @@ from .decorators import already_authenticated, auth_required
 from django.forms import formset_factory
 from django.contrib.auth.views import PasswordResetView
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.sites.shortcuts import get_current_site
+from django.utils.encoding import force_bytes, force_text
+from django.utils.http import urlsafe_base64_encode, urlsafe_base64_decode
+from django.template.loader import render_to_string
+from .tokens import account_activation_token
 
 from django.core.mail import send_mail
 
@@ -98,14 +103,49 @@ def register_users(request):
     if request.method == 'POST':
         form = CreateUsers(request.POST)
         if form.is_valid():
-            user = form.save()
-            username = form.cleaned_data.get('username')
-            group, created = Group.objects.get_or_create(name='Simple User')
-            user.groups.add(group)
-            messages.success(request, 'Account successfully created for ' + username)
-            return redirect('omnetppManager_login')
+            user = form.save(commit=False)
+            user.is_active = False
+            user.save()
+            current_site = get_current_site(request)
+            mail_subject = 'Activate your Omnetpp manager account.'
+            message = render_to_string('user_activation_email.html', {
+                'user': user,
+                'domain': current_site.domain,
+                'uid':urlsafe_base64_encode(force_bytes(user.pk)),
+                'token':account_activation_token.make_token(user),
+            })
+            to_email = form.cleaned_data.get('email')
+            send_mail(
+                        mail_subject,
+                        message,
+                        ConfigKeyValueStorage.config.get_value("DEFAULT_SENDER_MAIL_ADDRESS"),
+                        [to_email, ],
+                        fail_silently = False,
+                        )
+            return HttpResponse('Please confirm your email address to complete the registration !!!')
     context= {'form': form}
     return render(request,'registration/register_users.html', context)
+
+## activate user after confirmationx`
+def activate(request, uidb64, token):
+    try:
+        uid = force_text(urlsafe_base64_decode(uidb64))
+        user = User.objects.get(pk=uid)
+    except(TypeError, ValueError, OverflowError, User.DoesNotExist):
+        user = None
+    if user is not None and account_activation_token.check_token(user, token):
+        user.is_active = True
+        user.save()
+        # return redirect('home')
+
+        username = form.cleaned_data.get('username')
+        group, created = Group.objects.get_or_create(name='Simple User')
+        user.groups.add(group)
+        messages.success(request, 'Account successfully created for ' + username)
+        return redirect('omnetppManager_login')
+        # return HttpResponse('Thank you for your email confirmation. Now you can login your account.')
+    else:
+        return HttpResponse('Activation link is invalid!')
 
 
 ## User login
