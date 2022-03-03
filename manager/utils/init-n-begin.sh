43e8d403761e496b9724f05d41f7734dfb298a53
Asanga Udugama <adu@haumea.comnets.uni-bremen.de>
2022-03-03 14:29:30 +0100

    made to download traces by default

--- manager/utils/init-n-begin.sh
+++ manager/utils/init-n-begin.sh
@@ -7,20 +7,16 @@
 
 # Download and expand traces
 OPT_FOLDER="/opt"
-TRACES_FOLDER="/opt/data/traces"
 TRACES_ARCHIVE="ootb-traces.tar.gz"
 TRACES_LINK="https://seafile.zfn.uni-bremen.de/f/291602b767e642d49827/?dl=1"
-if [ ! -d "$TRACES_FOLDER" ]; then
-    echo "$TRACES_FOLDER not present. About to download and expand traces."
-    echo "Downloading..."
-    wget -q "$TRACES_LINK" -O "$TRACES_ARCHIVE"
-    echo "Download completed. Expanding ..."
-    tar -xzf "$TRACES_ARCHIVE" -C "$OPT_FOLDER"
-    rm "$TRACES_ARCHIVE"
-    echo "All traces downloaded and expanded."
-else
-    echo "$TRACES_FOLDER is present. Assuming all traces available."
-fi
+
+echo "About to download and expand traces into $OPT_FOLDER."
+echo "Downloading..."
+wget -q "$TRACES_LINK" -O "$TRACES_ARCHIVE"
+echo "Download completed. Expanding ..."
+tar -xzf "$TRACES_ARCHIVE" -C "$OPT_FOLDER"
+rm "$TRACES_ARCHIVE"
+echo "All traces downloaded and expanded."
 
 # Start redis
 echo "Starting REDIS client"
@@ -29,5 +25,7 @@ if [[ ! -v REDIS_URL ]] || [[ -z "$REDIS_URL" ]]; then
 else
     CONNECT_STR=${REDIS_URL}
 fi
+
+echo "Worker ready to execute OPS simulation jobs."
 
 rq worker --url ${CONNECT_STR}
